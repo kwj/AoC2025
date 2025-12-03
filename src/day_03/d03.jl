85793e9b683f1cd661be79805662bf354909a1c2
kwj <kwj@vlax.net>
2025-12-02 23:46:59 -0800

    Remove an unnecessary argument

--- src/day_03/d03.jl
+++ src/day_03/d03.jl
@@ -5,11 +5,11 @@ function parse_file(fname::String)
     map(lst -> parse.(Int, lst), split.(readlines(joinpath(@__DIR__, fname)), ""))
 end
 
-function find_max_joltage(line::AbstractVector{Int}, len::Int, rest::Int, left::Int, acc::Int)
+function find_max_joltage(line::AbstractVector{Int}, rest::Int, left::Int, acc::Int)
     iszero(rest) && return acc
 
     d, idx = findmax(@view line[left:end - rest + 1])
-    find_max_joltage(line, len, rest - 1, left + idx, acc * 10 + d)
+    find_max_joltage(line, rest - 1, left + idx, acc * 10 + d)
 end
 
 function d03_p1(fname::String = "input"; n_batts = 2)
@@ -17,7 +17,7 @@ function d03_p1(fname::String = "input"; n_batts = 2)
 
     joltage = 0
     for line in data
-        joltage += find_max_joltage(line, length(line), n_batts, 1, 0)
+        joltage += find_max_joltage(line, n_batts, 1, 0)
     end
 
     joltage
@@ -28,7 +28,7 @@ function d03_p2(fname::String = "input"; n_batts = 12)
 
     joltage = 0
     for line in data
-        joltage += find_max_joltage(line, length(line), n_batts, 1, 0)
+        joltage += find_max_joltage(line, n_batts, 1, 0)
     end
 
     joltage
