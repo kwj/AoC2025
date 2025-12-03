ea187ee5fba996c4863501e23295abb8efee67fd
kwj <kwj@vlax.net>
2025-12-03 06:36:41 -0800

    Use loops instead of recursion

--- src/day_03/d03.jl
+++ src/day_03/d03.jl
@@ -5,23 +5,28 @@ function parse_file(fname::String)
     map(lst -> parse.(Int, lst), collect.(readlines(joinpath(@__DIR__, fname))))
 end
 
-function find_max_joltage(bank::AbstractVector{Int}, rest::Int, left::Int, acc::Int)
-    iszero(rest) && return acc
+function find_max_joltage(bank::AbstractVector{Int}, n_batts::Int)
+    left = firstindex(bank)
+    acc = 0
+    for x = reverse(0:n_batts - 1)
+        d, idx = findmax(@view bank[left:end - x])
+        left += idx
+        acc = acc * 10 + d
+    end
 
-    d, idx = findmax(@view bank[left:end - rest + 1])
-    find_max_joltage(bank, rest - 1, left + idx, acc * 10 + d)
+    acc
 end
 
 function d03_p1(fname::String = "input"; n_batts = 2)
     data = parse_file(fname)
 
-    sum(map(bank -> find_max_joltage(bank, n_batts, 1, 0), data))
+    sum(map(bank -> find_max_joltage(bank, n_batts), data))
 end
 
 function d03_p2(fname::String = "input"; n_batts = 12)
     data = parse_file(fname)
 
-    sum(map(bank -> find_max_joltage(bank, n_batts, 1, 0), data))
+    sum(map(bank -> find_max_joltage(bank, n_batts), data))
 end
 
 end #module
