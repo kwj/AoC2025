7e5e43161ec12ef0c687b97c59857931068058f8
kwj <kwj@vlax.net>
2025-12-03 22:22:36 -0800

    Day 4

---
+++ src/day_04/d04.jl
@@ -0,0 +1,62 @@
+
+module Day04
+
+const CIdx = CartesianIndex
+const NBRS = CIdx.([(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)])
+
+function parse_file(fname::String)
+    map(ch -> ch == '@' ? 1 : 0, stack(collect.(readlines(joinpath(@__DIR__, fname))), dims = 1))
+end
+
+function num_adj_rolls(grid::Array{Int, 2}, pos::CIdx)
+    acc = 0
+    for dir in NBRS
+        acc += get(grid, pos + dir, 0)
+    end
+
+    acc
+end
+
+function find_rolls_to_remove(grid::Array{Int, 2}, thr::Int)
+    q = Vector{CIdx{2}}()
+
+    for pos in CartesianIndices(grid)
+        # empty space
+        grid[pos] == 0 && continue
+
+        if num_adj_rolls(grid, pos) < thr
+            push!(q, pos)
+        end
+    end
+
+    q
+end
+
+function d04_p1(fname::String = "input"; thr = 4)
+    grid = parse_file(fname)
+
+    find_rolls_to_remove(grid, thr) |> length
+end
+
+function d04_p2(fname::String = "input"; thr = 4)
+    grid = parse_file(fname)
+
+    acc = 0
+    while true
+        (rolls = find_rolls_to_remove(grid, thr)) |> isempty && break
+
+        # remove rolls of paper
+        for pos in rolls
+            grid[pos] = 0
+        end
+
+        acc += length(rolls)
+    end
+
+    acc
+end
+
+end #module
+
+using .Day04: d04_p1, d04_p2
+export d04_p1, d04_p2
