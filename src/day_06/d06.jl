fbd48dce4ccbfa88b585950da3ec3c37d142c30d
kwj <kwj@vlax.net>
2025-12-06 06:21:30 -0800

    Tweak

--- src/day_06/d06.jl
+++ src/day_06/d06.jl
@@ -5,9 +5,13 @@ function parse_file(fname::String)
     data = readlines(joinpath(@__DIR__, fname))
 
     matrix = stack(collect.(@view data[1:end - 1]), dims = 1)
+
+    sep_indices = findall(col -> all(==(' '), col), eachcol(matrix))
+    rngs = zip(vcat(1, sep_indices .+ 1), vcat(sep_indices .- 1, size(matrix, 2)))
+
     op_lst = map(s -> s == "+" ? (+) : (*), split(data[end]))
 
-    matrix, op_lst
+    matrix, rngs, op_lst
 end
 
 function chs_to_int(chs::AbstractArray{Char})
@@ -21,15 +25,13 @@ function chs_to_int(chs::AbstractArray{Char})
 end
 
 function d06(fname::String, fn::Function)
-    matrix, op_lst = parse_file(fname)
-    sep_indices = findall(col -> all(==(' '), col), eachcol(matrix))
-    rngs = zip(vcat(1, map(x -> x + 1, sep_indices)), vcat(map(x -> x - 1, sep_indices), size(matrix, 2)))
+    matrix, rngs, op_lst = parse_file(fname)
 
     q = Vector{Int}()
     idx = 1
     acc = 0
     for (start, stop) in rngs
-        for chs in eachrow(fn(@view matrix[:, start:stop]))
+        for chs in fn(@view matrix[:, start:stop])
             push!(q, chs_to_int(chs))
         end
         acc += reduce(op_lst[idx], q)
@@ -38,11 +40,10 @@ function d06(fname::String, fn::Function)
     end
 
     acc
-
 end
 
-d06_p1(fname::String = "input") = d06(fname, identity)
-d06_p2(fname::String = "input") = d06(fname, transpose)
+d06_p1(fname::String = "input") = d06(fname, eachrow)
+d06_p2(fname::String = "input") = d06(fname, eachcol)
 
 end #module
 
