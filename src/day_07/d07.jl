72ff99be766eb45c2b74ffd41ba09e3f96f1d9bd
kwj <kwj@vlax.net>
2025-12-06 23:21:26 -0800

    Switch to column-oriented style

--- src/day_07/d07.jl
+++ src/day_07/d07.jl
@@ -2,7 +2,7 @@
 module Day07
 
 function parse_file(fname::String)
-    stack(collect.(readlines(joinpath(@__DIR__, fname))), dims = 1)
+    stack(collect.(readlines(joinpath(@__DIR__, fname))))
 end
 
 function d07(fname::String)
@@ -12,21 +12,21 @@ function d07(fname::String)
     dp_tbl = zeros(Int, size(grid))
     dp_tbl[findfirst(==('S'), grid)] = 1
 
-    for r = 2:last(axes(grid, 1))
-        beam_col_indices = findall(!iszero, @view dp_tbl[r - 1, :])
+    for c = 2:last(axes(grid, 2))
+        beam_row_indices = findall(!iszero, @view dp_tbl[:, c - 1])
 
-        for c in beam_col_indices
+        for r in beam_row_indices
             if grid[r, c] == '^'
-                dp_tbl[r, c - 1] += dp_tbl[r - 1, c]
-                dp_tbl[r, c + 1] += dp_tbl[r - 1, c]
+                dp_tbl[r - 1, c] += dp_tbl[r, c - 1]
+                dp_tbl[r + 1, c] += dp_tbl[r, c - 1]
                 n_split += 1
             else
-                dp_tbl[r, c] += dp_tbl[r - 1, c]
+                dp_tbl[r, c] += dp_tbl[r, c - 1]
             end
         end
     end
 
-    n_split, sum(@view dp_tbl[end, :])
+    n_split, sum(@view dp_tbl[:, end])
 end
 
 d07_p1(fname::String = "input") = d07(fname)[1]
