058f6dd15e7c4f542845ef25f0cd2c5a3e01c7cc
kwj <kwj@vlax.net>
2026-01-11 13:57:25 -0800

    Make code simpler

--- src/day_11/d11.jl
+++ src/day_11/d11.jl
@@ -14,9 +14,9 @@ function parse_file(fname::String)
 end
 
 # part one
-function count_paths(d::String, memo::Dict{String, Int}, devs::Dict{String, Vector{String}})
+function count_paths(devs::Dict{String, Vector{String}}, d::String, memo = Dict{String, Int}())
     get!(memo, d) do
-        d == "out" ? 1 : sum(next_d -> count_paths(next_d, memo, devs), devs[d])
+        d == "out" ? 1 : sum(next_d -> count_paths(devs, next_d, memo), devs[d])
     end
 end
 
@@ -29,7 +29,7 @@ end
 
 State(x::State; dev_name = x.dev_name, dac = x.dac, fft = x.fft) = State(dev_name, dac, fft)
 
-function count_paths(st::State, memo::Dict{State, Int}, devs::Dict{String, Vector{String}})
+function count_paths(devs::Dict{String, Vector{String}}, st::State, memo = Dict{State, Int}())
     get!(memo, st) do
         if st.dev_name == "out"
             (st.dac && st.fft) ? 1 : 0
@@ -43,16 +43,14 @@ function count_paths(st::State, memo::Dict{State, Int}, devs::Dict{String, Vecto
             end
 
             sum(devs[st.dev_name]) do next_d
-                count_paths(State(st, dev_name = next_d), memo, devs)
+                count_paths(devs, State(st, dev_name = next_d), memo)
             end
         end
     end
 end
 
-d11(fname::String, aux::Function, start) = aux(start, Dict{typeof(start), Int}(), parse_file(fname))
-
-d11_p1(fname::String = "input") = d11(fname, count_paths, "you")
-d11_p2(fname::String = "input") = d11(fname, count_paths, State("svr", false, false))
+d11_p1(fname::String = "input") = count_paths(parse_file(fname), "you")
+d11_p2(fname::String = "input") = count_paths(parse_file(fname), State("svr", false, false))
 
 end #module
 
